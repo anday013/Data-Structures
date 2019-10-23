d4251c46d1c6f66bbe4a4b16294acbb61251b5da
Anday Ismayilzade <ipman4819@gmail.com>
2019-10-23 19:31:38 +0400

    All except ChangeOrder

--- Playlist_with_DLL.c
+++ Playlist_with_DLL.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 struct Track
 {
     char *title;
     char *genre;
     char *artist;
-    int duration;
+    double duration;
 } typedef Track;
 
 struct TrackNode
@@ -40,6 +41,11 @@ void PLaylist_shuffle(Playlist **playlist);
 void Playlist_move2(Playlist **playlist1, Playlist **playlist, char *title);
 TrackNode *Playlist_findNode(Playlist *playlist, char *title);
 void Playlist_changeorder(Playlist **playlist, char *TTitle, int order);
+void Playlist_sort_byTitle(Playlist **playlist);
+static int myCompare(const void *a, const void *b);
+void sort(const char *arr[], int n);
+void swap(char **a, char **b);
+void randomize(const char *arr[], int n);
 // Track_new(); +
 // TrackNode_new(track); +
 // Playlist_new(Title); +
@@ -53,21 +59,35 @@ void Playlist_changeorder(Playlist **playlist, char *TTitle, int order);
 
 int main()
 {
-    Track *track1 = Track_new("Wish you were here", "Rock", "Pink Floyd", 240);
-    Track *track2 = Track_new("Elif Dedim", "Turku", "Halk", 9999);
-    Track *track3 = Track_new("Hekimoglu", "Turku", "Cakir", 42);
-    Track *track4 = Track_new("Eysan", "OC", "Omer", 3);
-    Playlist *playlist = Playlist_new("Kurtlar Vadisi");
-    Playlist *playlist2 = Playlist_new("Ezel");
+    Track *track1 = Track_new("Wish you were here", "Rock", "Pink Floyd", 5);
+    Track *track2 = Track_new("Bohemian Rhapsody", "Rock", "Queen", 6);
+    Track *track3 = Track_new("We Will Rock You", "Rock", "Queen", 4);
+    Track *track4 = Track_new("Don't Stop Me Now", "Rock", "Queen", 3);
+    Track *track5 = Track_new("Havana", "Pop", "Camila Cabello", 3.5);
+    Track *track6 = Track_new("Happy", "Pop", "Pharrell Williams", 4.5);
+    Playlist *playlist = Playlist_new("Mix");
+    Playlist *playlist2 = Playlist_new("Rock");
     Playlist_append(&playlist, track1);
+    Playlist_append(&playlist2, track1);
     Playlist_append(&playlist, track2);
+    Playlist_append(&playlist2, track2);
     Playlist_append(&playlist, track3);
+    Playlist_append(&playlist2, track3);
     Playlist_append(&playlist, track4);
+    Playlist_append(&playlist2, track4);
+    Playlist_append(&playlist, track5);
+    Playlist_append(&playlist, track6);
     // printf("\nInitial version: \n");
+    printf("\n*********************************************\n");
     Playlist_play(playlist);
+    printf("\n*********************************************\n");
+    // Playlist_sort_byTitle(&playlist);
+    PLaylist_shuffle(&playlist);
     // Playlist_changeorder(&playlist, track2->title, 1);
     // printf("\nAfter changeOrder: \n");
-    // Playlist_play(playlist);
+    printf("\n*********************************************\n");
+    Playlist_play(playlist);
+    printf("\n*********************************************\n");
     // Playlist_append(&playlist2, track4);
     // printf("***************************************\n");
     // Playlist_play(playlist);
@@ -131,14 +151,6 @@ void Playlist_append(Playlist **playlist, Track *TTrack)
         (*playlist)->numOfTracks++;
         return;
     }
-    // if ((*playlist)->tail == NULL)
-    // {
-    //     (*playlist)->tail = newTrackNode;
-    //     (*playlist)->tail->prevTrack = (*playlist)->head;
-    //     (*playlist)->head->nextTrack = (*playlist)->tail;
-    //     (*playlist)->totalDuration += newTrackNode->track->duration;
-    //     return;
-    // }
     (*playlist)->tail->nextTrack = newTrackNode;
     newTrackNode->prevTrack = (*playlist)->tail;
     (*playlist)->tail = newTrackNode;
@@ -152,6 +164,7 @@ void Playlist_remove(Playlist **playlist, char *TTitle)
     TrackNode *temp = (*playlist)->head;
     if (strcmp((*playlist)->head->track->title, TTitle) == 0)
     {
+        (*playlist)->head->nextTrack->prevTrack = NULL;
         (*playlist)->head = (*playlist)->head->nextTrack;
         (*playlist)->totalDuration -= (*playlist)->head->track->duration;
         (*playlist)->numOfTracks--;
@@ -204,21 +217,19 @@ void Playlist_play(Playlist *playlist)
 
 void Playlist_clear(Playlist **playlist)
 {
-    if ((*playlist)->head == NULL)
+
+    if ((*playlist)->head == NULL || (*playlist)->tail == NULL)
     {
         printf("Playlist '%s' is invalid!\n", (*playlist)->title);
         return;
     }
-    if ((*playlist)->tail == NULL)
+    while ((*playlist)->head->nextTrack != NULL)
     {
-        printf("Playlist '%s' is invalid!\n", (*playlist)->title);
-        return;
+        free((*playlist)->head);
     }
-    (*playlist)->head = NULL;
-    (*playlist)->tail = NULL;
-    if ((*playlist)->currentTrack != NULL)
+    if ((*playlist)->currentTrack != NULL || (*playlist)->currentTrack == NULL)
     {
-        (*playlist)->currentTrack = NULL;
+        free((*playlist)->currentTrack);
     }
     (*playlist)->currentDuration = 0;
     (*playlist)->numOfTracks = 0;
@@ -277,7 +288,7 @@ void Playlist_changeorder(Playlist **playlist, char *TTitle, int order)
         (*playlist)->head = (*playlist)->head->nextTrack;
     }
 
-    if(newTrackNode == NULL) 
+    if (newTrackNode == NULL)
     {
         printf("Track title couldn't been found in playlist!\n");
         return;
@@ -294,7 +305,7 @@ void Playlist_changeorder(Playlist **playlist, char *TTitle, int order)
             (*playlist)->head = (*playlist)->head->nextTrack;
             counter++;
         }
-        if(order == 1)
+        if (order == 1)
         {
             newTrackNode->nextTrack = (*playlist)->head;
             (*playlist)->head->nextTrack = (*playlist)->head->nextTrack->nextTrack;
@@ -304,5 +315,104 @@ void Playlist_changeorder(Playlist **playlist, char *TTitle, int order)
         newTrackNode->nextTrack = (*playlist)->head->nextTrack;
         (*playlist)->head->nextTrack = newTrackNode;
         (*playlist)->head = temp;
+    }
+}
+
+void Playlist_sort_byTitle(Playlist **playlist)
+{
+    int numOfTracks = (*playlist)->numOfTracks;
+    TrackNode *temp = (*playlist)->head;
+    const char *arr[numOfTracks];
+    if ((*playlist)->head == NULL)
+    {
+        printf("Playlist is empty.\n");
+        return;
+    }
+    int i = 0;
+    while ((*playlist)->head != NULL)
+    {
+        arr[i++] = (*playlist)->head->track->title;
+        // printf("%s\n", (*playlist)->head->track->title);
+        (*playlist)->head = (*playlist)->head->nextTrack;
+    }
+    (*playlist)->head = temp;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    sort(arr, n);
+    i = 0;
+    while (i < numOfTracks)
+    {
+        char *name = (char *)arr[i++];
+        TrackNode *newTrackNode = Playlist_findNode((*playlist), name);
+        Playlist_remove(playlist, newTrackNode->track->title);
+        Playlist_append(playlist, newTrackNode->track);
+    }
+}
+
+
+
+
+
+void PLaylist_shuffle(Playlist **playlist) {
+	int numOfTracks = (*playlist)->numOfTracks;
+    TrackNode *temp = (*playlist)->head;
+
+	const char* arr[numOfTracks];
+	if ((*playlist)->head == NULL)
+    {
+        printf("Playlist is empty.\n");
+        return;
+    }
+
+    int i=0;
+    while ((*playlist)->head != NULL)
+    {
+    	arr[i++] = (*playlist)->head->track->title;
+        (*playlist)->head = (*playlist)->head->nextTrack;
+    }
+    (*playlist)->head = temp;
+
+	int n = sizeof(arr) / sizeof(arr[0]);
+	randomize(arr, n);
+
+	i=0;
+    while(i<numOfTracks){
+    	char* name = (char *)arr[i++];
+    	TrackNode *newTrackNode = Playlist_findNode((*playlist), name);
+    	Playlist_remove(playlist, newTrackNode->track->title);
+    	Playlist_append(playlist, newTrackNode->track);
+    }
+}
+
+
+
+
+
+
+
+
+static int myCompare(const void *a, const void *b)
+{
+    return strcmp(*(const char**)a, *(const char**)b); 
+}
+
+void sort(const char *arr[], int n)
+{
+    qsort(arr, n, sizeof(const char *), myCompare);
+}
+
+void swap(char **a, char **b)
+{
+    char *temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void randomize(const char *arr[], int n)
+{
+    srand(time(NULL));
+    for (int i = n - 1; i > 0; i--)
+    {
+        int j = rand() % (i + 1);
+        swap((char **)&arr[i], (char **)&arr[j]);
     }
 }
